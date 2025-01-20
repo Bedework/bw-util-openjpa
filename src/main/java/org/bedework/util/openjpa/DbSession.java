734aebf8027bfa869ad06e4b0a6ed38f9b688058
mike <mikeadouglass@gmail.com>
2025-01-19 23:45:54 -0500

    Add start of openjpa implementation. Add new database exception and move db related exceptions into new package. Get rid of HibException and use above. Remove some db interface features we never used: * getBlob(InputStream val, long length); * reattach * webMode which used hibernate specific flushmode * createNoFlushQuery * getQueryString * saveOrUpdateCopy * restore * reAttach * lockRead * lockUpdate

---
+++ src/main/java/org/bedework/util/openjpa/DbSession.java
@@ -0,0 +1,249 @@
+/* ********************************************************************
+    Licensed to Jasig under one or more contributor license
+    agreements. See the NOTICE file distributed with this work
+    for additional information regarding copyright ownership.
+    Jasig licenses this file to you under the Apache License,
+    Version 2.0 (the "License"); you may not use this file
+    except in compliance with the License. You may obtain a
+    copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations
+    under the License.
+*/
+package org.bedework.util.openjpa;
+
+import java.io.Serializable;
+import java.sql.Blob;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/** Interface to do hibernate interactions.
+ *
+ * @author Mike Douglass douglm at rpi.edu
+ */
+public interface DbSession extends Serializable {
+  /** Set up for a hibernate interaction. Throw the object away on exception.
+   *
+   * @param factory
+   */
+  void init(EntityManagerFactory factory);
+
+  /**
+   * @return Session
+   */
+  EntityManager getSession();
+
+  /**
+   * @return boolean true if open
+   */
+  boolean isOpen();
+
+  /** If we had a database exception this will return non-null. The session
+   * needs to be discarded.
+   *
+   * @return current exception or null.
+   */
+  Throwable getException();
+
+  /** Begin a transaction
+   *
+   */
+  void beginTransaction();
+
+  /** Return true if we have a transaction started
+   *
+   * @return boolean
+   */
+  boolean transactionStarted();
+
+  /** Commit a transaction
+   *
+   */
+  void commit();
+
+  /** Rollback a transaction
+   *
+   */
+  void rollback();
+
+  /** Did we rollback the transaction?
+   *
+   * @return boolean
+   */
+  boolean rolledback();
+
+  /**
+   * @return a timestamp from the db
+   */
+  Timestamp getCurrentTimestamp();
+
+  /**
+   * @return a blob
+   */
+  Blob getBlob(byte[] val);
+
+  /** Evict an object from the session.
+   *
+   * @param val          Object to evict
+   */
+  void evict(Object val);
+
+  /** Create a query ready for parameter replacement or execution.
+   *
+   * @param s             String hibernate query
+   */
+  void createQuery(String s);
+
+  /** Mark the query as cacheable
+   *
+   */
+  void cacheableQuery();
+
+  /** Set the named parameter with the given value
+   *
+   * @param parName     String parameter name
+   * @param parVal      String parameter value
+   */
+  void setString(String parName, String parVal);
+
+  /** Set the named parameter with the given value
+   *
+   * @param parName     String parameter name
+   * @param parVal      boolean parameter value
+   */
+  void setBool(String parName, boolean parVal);
+
+  /** Set the named parameter with the given value
+   *
+   * @param parName     String parameter name
+   * @param parVal      int parameter value
+   */
+  void setInt(String parName, int parVal);
+
+  /** Set the named parameter with the given value
+   *
+   * @param parName     String parameter name
+   * @param parVal      long parameter value
+   */
+  void setLong(String parName, long parVal);
+
+  /** Set the named parameter with the given value
+   *
+   * @param parName     String parameter name
+   * @param parVal      Object parameter value
+   */
+  void setEntity(String parName, Object parVal);
+
+  /** Set the named parameter with the given Collection
+   *
+   * @param parName     String parameter name
+   * @param parVal      Collection parameter value
+   */
+  void setParameterList(String parName,
+                        Collection<?> parVal);
+
+  /** Set the first result for a paged batch
+   *
+   * @param val      int first index
+   */
+  void setFirstResult(int val);
+
+  /** Set the max number of results for a paged batch
+   *
+   * @param val      int max number
+   */
+  void setMaxResults(int val);
+
+  /** Return the single object resulting from the query.
+   *
+   * @return Object          retrieved object or null
+   */
+  Object getUnique();
+
+  /** Return a list resulting from the query.
+   *
+   * @return List          list from query
+   */
+  List<?> getList();
+
+  /**
+   * @return int number updated
+   */
+  int executeUpdate();
+
+  /** Update an object which may have been loaded in a previous hibernate
+   * session
+   *
+   * @param obj
+   */
+  void update(Object obj);
+
+  /** Merge and update an object which may have been loaded in a previous hibernate
+   * session
+   *
+   * @param obj
+   * @return Object   the persiatent object
+   */
+  Object merge(Object obj);
+
+  /** Save a new object or update an object which may have been loaded in a
+   * previous hibernate session
+   *
+   * @param obj
+   */
+  void saveOrUpdate(Object obj);
+
+  /** Return an object of the given class with the given id if it is
+   * already associated with this session. This must be called for specific
+   * key queries or we can get a NonUniqueObjectException later.
+   *
+   * @param  cl    Class of the instance
+   * @param  id    A serializable key
+   * @return Object
+   */
+  Object get(Class<?> cl, Serializable id);
+
+  /** Return an object of the given class with the given id if it is
+   * already associated with this session. This must be called for specific
+   * key queries or we can get a NonUniqueObjectException later.
+   *
+   * @param  cl    Class of the instance
+   * @param  id    int key
+   * @return Object
+   */
+  Object get(Class<?> cl, int id);
+
+  /** Save a new object.
+   *
+   * @param obj
+   */
+  void save(Object obj);
+
+  /** Delete an object
+   *
+   * @param obj
+   */
+  void delete(Object obj);
+
+  /**
+   */
+  void flush();
+
+  /**
+   */
+  void clear();
+
+  /**
+   */
+  void close();
+}
---
+++ src/main/java/org/bedework/util/openjpa/DbSessionImpl.java
@@ -0,0 +1,735 @@
+/* ********************************************************************
+    Licensed to Jasig under one or more contributor license
+    agreements. See the NOTICE file distributed with this work
+    for additional information regarding copyright ownership.
+    Jasig licenses this file to you under the Apache License,
+    Version 2.0 (the "License"); you may not use this file
+    except in compliance with the License. You may obtain a
+    copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations
+    under the License.
+*/
+package org.bedework.util.openjpa;
+
+import org.bedework.base.exc.BedeworkException;
+import org.bedework.base.exc.persist.BedeworkConstraintViolationException;
+import org.bedework.base.exc.persist.BedeworkDatabaseException;
+import org.bedework.base.exc.persist.BedeworkStaleStateException;
+import org.bedework.util.logging.BwLogger;
+import org.bedework.util.logging.Logged;
+import org.bedework.util.misc.Util;
+
+import org.apache.openjpa.persistence.OpenJPAEntityManager;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.sql.Blob;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.jms.Session;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.FlushModeType;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.Query;
+import javax.transaction.Transaction;
+
+/** Convenience class to do the actual hibernate interaction. Intended for
+ * one use only.
+ *
+ * @author Mike Douglass douglm@rpi.edu
+ */
+public class DbSessionImpl implements Logged, DbSession {
+  EntityManager sess;
+  transient EntityTransaction tx;
+  boolean rolledBack;
+
+  transient Query q;
+
+  /** Exception from this session. */
+  BedeworkException exc;
+
+  private final SimpleDateFormat dateFormatter =
+          new SimpleDateFormat("yyyy-MM-dd");
+
+  @Override
+  public void init(final EntityManagerFactory factory) {
+    try {
+      sess = factory.createEntityManager();
+      rolledBack = false;
+    } catch (final Throwable t) {
+      exc = new BedeworkDatabaseException(t);
+      tx = null;  // not even started. Should be null anyway
+      close();
+    }
+  }
+
+  @Override
+  public EntityManager getSession() {
+    return sess;
+  }
+
+  @Override
+  public boolean isOpen() {
+    try {
+      if (sess == null) {
+        return false;
+      }
+      return sess.isOpen();
+    } catch (final Throwable t) {
+      handleException(t);
+      return false;
+    }
+  }
+
+  @Override
+  public Throwable getException() {
+    return exc;
+  }
+
+  @Override
+  public void beginTransaction() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      if (tx != null) {
+        exc = new BedeworkDatabaseException("Transaction already started");
+        throw exc;
+      }
+
+      tx = sess.getTransaction();
+      rolledBack = false;
+      if (tx == null) {
+        exc = new BedeworkDatabaseException("Transaction not started");
+        throw exc;
+      }
+      tx.begin();
+    } catch (final BedeworkException be) {
+      exc = be;
+      throw be;
+    } catch (final Throwable t) {
+      exc = new BedeworkDatabaseException(t);
+      throw exc;
+    }
+  }
+
+  @Override
+  public boolean transactionStarted() {
+    return tx != null;
+  }
+
+  @Override
+  public void commit() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+//      if (tx != null &&
+//          !tx.wasCommitted() &&
+//          !tx.wasRolledBack()) {
+        //if (getLogger().isDebugEnabled()) {
+        //  getLogger().debug("About to comnmit");
+        //}
+      if ((tx != null) &&
+              !rolledBack &&
+              !tx.getRollbackOnly()) {
+        tx.commit();
+      }
+
+      tx = null;
+    } catch (final Throwable t) {
+      if (t instanceof OptimisticLockException) {
+        exc = new BedeworkStaleStateException(t);
+        throw exc;
+      }
+
+      final Class<?> obj;
+      try {
+        obj = t.getClass().getClassLoader().loadClass("javax.persistence.OptimisticLockException");
+      } catch (final ClassNotFoundException cnfe) {
+        exc = new BedeworkDatabaseException(t);
+        throw exc;
+      }
+      if (t.getClass().isAssignableFrom(obj)) {
+        exc = new BedeworkStaleStateException(t);
+        throw exc;
+      }
+
+      exc = new BedeworkDatabaseException(t);
+      throw exc;
+    }
+  }
+
+  @Override
+  public void rollback() {
+/*    if (exc != null) {
+      // Didn't hear me last time?
+      throw  new BedeworkDatabaseException(exc);
+    }
+*/
+    if (getLogger().isDebugEnabled()) {
+      getLogger().debug("Enter rollback");
+    }
+    try {
+      if ((tx != null) &&
+          !rolledBack) {
+        if (getLogger().isDebugEnabled()) {
+          getLogger().debug("About to rollback");
+        }
+        tx.rollback();
+        tx = null;
+        sess.clear();
+      }
+    } catch (final Throwable t) {
+      exc = new BedeworkDatabaseException(t);
+      throw exc;
+    } finally {
+      rolledBack = true;
+    }
+  }
+
+  @Override
+  public boolean rolledback() {
+    return rolledBack;
+  }
+
+  private static final String getCurrentTimestampQuery =
+      "select current_timestamp() from " + BwSystem.class.getName();
+
+  @Override
+  public Timestamp getCurrentTimestamp() {
+    try {
+      final List<?> l = sess.createQuery(getCurrentTimestampQuery)
+                            .getResultList();
+
+      if (Util.isEmpty(l)) {
+        return null;
+      }
+
+      return (Timestamp)l.get(0);
+    } catch (final Throwable t) {
+      handleException(t);
+      return null;
+    }
+  }
+
+  @Override
+  public Blob getBlob(final byte[] val) {
+    return Hibernate.getLobCreator(sess).createBlob(val);
+  }
+
+  @Override
+  public void evict(final Object val) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw  new BedeworkDatabaseException(exc);
+    }
+
+    try {
+      ((OpenJPAEntityManager)sess).evict(val);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void createQuery(final String s) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q = sess.createQuery(s);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void cacheableQuery() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setCacheable(true);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setString(final String parName, final String parVal) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setParameter(parName, parVal);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setBool(final String parName, final boolean parVal) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setParameter(parName, parVal);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setInt(final String parName, final int parVal) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setParameter(parName, parVal);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setLong(final String parName, final long parVal) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setParameter(parName, parVal);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setEntity(final String parName, final Object parVal) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setEntity(parName, parVal);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setParameterList(final String parName,
+                               final Collection<?> parVal) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setParameter(parName, parVal);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setFirstResult(final int val) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setFirstResult(val);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void setMaxResults(final int val) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      q.setMaxResults(val);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public Object getUnique() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      return q.getSingleResult();
+    } catch (final Throwable t) {
+      handleException(t);
+      return null;  // Don't get here
+    }
+  }
+
+  @Override
+  public List<?> getList() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      final List<?> l = q.getResultList();
+
+      if (l == null) {
+        return new ArrayList<>();
+      }
+
+      return l;
+    } catch (final Throwable t) {
+      handleException(t);
+      return null;  // Don't get here
+    }
+  }
+
+  @Override
+  public int executeUpdate() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    if (q == null) {
+      exc = new BedeworkDatabaseException("No query for execute update");
+      throw exc;
+    }
+
+    try {
+      return q.executeUpdate();
+    } catch (final Throwable t) {
+      handleException(t);
+      return 0;  // Don't get here
+    }
+  }
+
+  @Override
+  public void update(final Object obj) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      beforeSave(obj);
+      sess.merge(obj);
+      deleteSubs(obj);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public Object merge(Object obj) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      beforeSave(obj);
+
+      obj = sess.merge(obj);
+      deleteSubs(obj);
+
+      return obj;
+    } catch (final Throwable t) {
+      handleException(t, obj);
+      return null;
+    }
+  }
+
+  @Override
+  public void saveOrUpdate(final Object obj) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      beforeSave(obj);
+
+      sess.merge(obj);
+      deleteSubs(obj);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public Object get(final Class<?> cl,
+                    final Serializable id) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      return sess.find(cl, id);
+    } catch (final Throwable t) {
+      handleException(t);
+      return null;  // Don't get here
+    }
+  }
+
+  @Override
+  public Object get(final Class<?> cl, final int id) {
+    return get(cl, Integer.valueOf(id));
+  }
+
+  @Override
+  public void save(final Object obj) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      beforeSave(obj);
+      sess.persist(obj);
+      deleteSubs(obj);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void delete(final Object obj) {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    try {
+      beforeDelete(obj);
+
+      evict(obj);
+      sess.remove(sess.merge(obj));
+      deleteSubs(obj);
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void flush() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    if (getLogger().isDebugEnabled()) {
+      getLogger().debug("About to flush");
+    }
+    try {
+      sess.flush();
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  @Override
+  public void clear() {
+    if (exc != null) {
+      // Didn't hear me last time?
+      throw exc;
+    }
+
+    if (getLogger().isDebugEnabled()) {
+      getLogger().debug("About to flush");
+    }
+    try {
+      sess.clear();
+    } catch (final Throwable t) {
+      handleException(t);
+    }
+  }
+
+  /**
+   */
+  @Override
+  public void close() {
+    if (sess == null) {
+      return;
+    }
+
+//    throw  new BedeworkDatabaseException("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");/*
+    try {
+      if (!rolledback() && sess.isDirty()) {
+        sess.flush();
+      }
+      if ((tx != null) && !rolledback()) {
+        tx.commit();
+      }
+    } catch (final Throwable t) {
+      if (exc == null) {
+        exc = new BedeworkException(t);
+      }
+    } finally {
+      tx = null;
+      if (sess != null) {
+        try {
+          sess.close();
+        } catch (final Throwable ignored) {}
+      }
+    }
+
+    sess = null;
+    if (exc != null) {
+      throw exc;
+    }
+//    */
+  }
+
+  private void handleException(final Throwable t) {
+    handleException(t, null);
+  }
+
+  private void handleException(final Throwable t,
+                               final Object o) {
+    try {
+      if (debug()) {
+        debug("handleException called");
+        if (o != null) {
+          debug(o.toString());
+        }
+        error(t);
+      }
+    } catch (final Throwable ignored) {}
+
+    try {
+      if (tx != null) {
+        try {
+          tx.rollback();
+        } catch (final Throwable t1) {
+          rollbackException(t1);
+        }
+        tx = null;
+      }
+    } finally {
+      try {
+        sess.close();
+      } catch (final Throwable ignored) {}
+      sess = null;
+    }
+
+    if (t instanceof final BedeworkException be) {
+      exc = be;
+      throw exc;
+    }
+
+    if (t instanceof OptimisticLockException) {
+      exc = new BedeworkStaleStateException(t);
+      throw exc;
+    }
+
+    /*
+    if (t instanceof ConstraintViolationException) {
+      throw new BedeworkConstraintViolationException(t);
+    }*/
+
+    exc = new BedeworkDatabaseException(t);
+    throw exc;
+  }
+
+  private void beforeSave(final Object o) {
+    if (!(o instanceof final VersionedDbEntity<?, ?> ent)) {
+      return;
+    }
+
+    ent.beforeSave();
+  }
+
+  private void beforeDelete(final Object o) {
+    if (!(o instanceof VersionedDbEntity)) {
+      return;
+    }
+
+    final var ent = (VersionedDbEntity<?, ?>)o;
+
+    ent.beforeDeletion();
+  }
+
+  private void deleteSubs(final Object o) {
+    if (!(o instanceof final VersionedDbEntity<?, ?> ent)) {
+      return;
+    }
+
+    final var subs = ent.getDeletedEntities();
+    if (subs == null) {
+      return;
+    }
+
+    for (final var sub: subs) {
+      evict(sub);
+      delete(sub);
+    }
+  }
+
+  /** This is just in case we want to report rollback exceptions. Seems we're
+   * likely to get one.
+   *
+   * @param t   Throwable from the rollback
+   */
+  private void rollbackException(final Throwable t) {
+    error(t);
+  }
+
+  /* ====================================================================
+   *                   Logged methods
+   * ==================================================================== */
+
+  private final BwLogger logger = new BwLogger();
+
+  @Override
+  public BwLogger getLogger() {
+    if ((logger.getLoggedClass() == null) && (logger.getLoggedName() == null)) {
+      logger.setLoggedClass(getClass());
+    }
+
+    return logger;
+  }
+}
